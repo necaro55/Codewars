2dabe997ceacab485a9e0b584a24995c424569fd
Nestor Caceres <necaro55@gmail.com>
2021-01-05 20:19:12 -0600

    Update Are_they_the_same.py
    Added description

--- Are_they_the_same.py
+++ Are_they_the_same.py
@@ -1,3 +1,5 @@
+#Given two arrays a and b write a function comp(a, b) (compSame(a, b) in Clojure) that checks whether the two arrays have the "same" elements, with the same multiplicities. "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
+
 def comp(array1, array2):
     
     if array1 == None or array2 == None:
