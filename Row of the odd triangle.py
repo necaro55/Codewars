b192ec470b9b02bd604ac22f2a5563727280734a
Nestor Caceres <necaro55@gmail.com>
2021-01-06 18:58:19 -0600

    Update Row of the odd triangle.py

--- Row of the odd triangle.py
+++ Row of the odd triangle.py
@@ -1,3 +1,17 @@
+#Given a triangle of consecutive odd numbers:
+
+#             1
+#          3     5
+#       7     9    11
+#   13    15    17    19
+#21    23    25    27    29
+#...
+#find the triangle's row knowing its index (the rows are 1-indexed), e.g.:
+
+#odd_row(1)  ==  [1]
+#odd_row(2)  ==  [3, 5]
+#odd_row(3)  ==  [7, 9, 11]
+
 def odd_row(n):
     
     place_of_first_odd = int(((n-1)*(n))/2) + 1
